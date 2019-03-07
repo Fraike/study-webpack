157e60cba6a18fce146d1e1e0ce08c8e8efdf116
wwt <648128145@qq.com>
2019-03-07 16:30:31 +0800

    update

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -8,62 +8,82 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.(jpg|png|gif)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    name: '[name]_[hash].[ext]', //placeholder占位符
-                    outputPath: 'images/',
-                    limit: 20480
+                test: /\.(jpg|png|gif)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name]_[hash].[ext]', //placeholder占位符
+                        outputPath: 'images/',
+                        limit: 20480
+                    }
                 }
-            }
-        },
-        {
-            test: /\.scss$/,
-            use: ['style-loader',
+            },
             {
-                loader: 'css-loader',
-                options: {
-                    importLoaders: 2
+                test: /\.scss$/,
+                use: ['style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2
+                        }
+                    }, 'sass-loader', 'postcss-loader'
+                ] //loader执行顺序从下到上从右到左
+            },
+            {
+                test: /\.(eot|ttf|svg|woff)$/,
+                use: {
+                    loader: 'file-loader'
                 }
-            },'sass-loader','postcss-loader'] //loader执行顺序从下到上从右到左
-        },
-        {
-            test: /\.(eot|ttf|svg|woff)$/,
-            use: {
-                loader: 'file-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader' //webpack和balel之间的桥梁
             }
-        },
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'postcss-loader'
-            ]
-        },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader' //webpack和balel之间的桥梁
-        }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-        template: 'src/index.html'
+            template: 'src/index.html'
         }),
-        new CleanWebpackPlugin(['dist'],{
-            root: path.resolve(__dirname,'../')
+        new CleanWebpackPlugin(['dist'], {
+            root: path.resolve(__dirname, '../')
         })
-    ], 
+    ],
     optimization: {
         splitChunks: {
-            chunks: 'all'
+            chunks: 'all', //代码分割只对异步代码有效果
+            minSize: 30000, //大于30kb就分割
+            minChunks: 1, //至少用了多少次 才进行代码分割
+            maxAsyncRequests: 5, //文件里面请求超过五次的就不分割了
+            maxInitialRequests: 3, //同上，只是是在首页
+            automaticNameDelimiter: '~', //连接符 不配置filename 就用vendor~文件名
+            name: true, //让cacheGroups内的filename有效
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/, //在node_modules中的
+                    priority: -10, //优先级
+                    // filename: 'vender.js'
+                },
+                default: {
+                    priority: -20, //优先级更低
+                    reuseExistingChunk: true, //
+                    filename: 'common.js'
+                }
+            }
         }
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname,'../dist')
+        chunkFilename: '[name].chunk.js',
+        path: path.resolve(__dirname, '../dist')
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import _ from 'lodash';
+async function getComponent() {
+    const {
+        default: _
+    } = await import('lodash')
+    var element = document.createElement('div')
+    element.innerHTML = _.join(['wwt', 'syj'], '-')
+    return element
+}
 
-console.log(_.join(['a','b','c'],'-'))
+document.addEventListener('click', () => {
+
+    getComponent().then(element => {
+        document.body.appendChild(element)
+    })
+})
+
+// import test from './a.js'
+
+// console.log(test.name)
+// import _ from 'lodash'
+
+// console.log(_.join(['wwt','syj'],'-'))
